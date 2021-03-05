df01376183cbe7ee82a797ff66297e2fce32db49
goro-tozawa <ralugoro.0905@icloud.com>
2021-03-05 15:48:36 +0900

    トップページのアニメーション修正

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,7 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
-require("../script")
+// require("../script")
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
@@ -15,3 +15,15 @@ require("../script")
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+document.addEventListener('DOMContentLoaded', (event) => {
+  const targetElement = document.querySelectorAll(".animationTarget");
+  document.getElementById("animation");
+  document.addEventListener("scroll", function() {
+    for (let i = 0; i < targetElement.length; i++) {
+      const getElementDistance = targetElement[i].getBoundingClientRect().top + targetElement[i].clientHeight * .6
+      if (window.innerHeight > getElementDistance) {
+        targetElement[i].classList.add("show");
+      }
+    }
+  })
+})
